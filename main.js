8fbac8c18013a0ab4794a6b34d331eec66a6084c
Ken Karlson <kenkarlson@mail.ru>
2024-05-13 14:53:26 +0300

    Add color elements

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function play() {
+  const button =document.querySelector(".button");
+  let txt = document.querySelector(".txt");
+
+
 function getZoo(year) {
   let zoo = '';
   let numberOfYear = Number(year)%12;
@@ -19,14 +23,37 @@ function getZoo(year) {
   }
   return zoo;
 }
+function getColor(year, info) {
+  info = 0;
+  let color = '';
+  let element = '';
+  let numberOfYear = Number(year)%10;
+  switch (numberOfYear) {
+    case 0:color='#ffffff'; element ='металл'; break;
+    case 1:color='#ffffff'; element ='металл'; break; 
+    case 2:color='black'; element ='вода'; break; 
+    case 3:color='black'; element ='вода'; break; 
+    case 4:color='green'; element ='дерево'; break;
+    case 5:color='green'; element ='дерево'; break;
+    case 6:color='red'; element ='огонь'; break;
+    case 7:color='red'; element ='огонь'; break;
+    case 8:color='saddlebrown'; element ='земля'; break;
+    case 9:color='saddlebrown'; element ='земля'; break;
+  }
+  if (info===0) {
+    return color;
+  }else {
+    return color +'' + element;
+  }
+}
 
-  const button =document.querySelector(".button");
-  let txt = document.querySelector(".txt");
+
   button.addEventListener("click", (event) => {  
     event.preventDefault();
     let year = +prompt("Enter Year");
     //getZoo(year);
     txt.innerHTML = getZoo(year);
+    txt.style.color = getColor(year);
   });
 }
 
